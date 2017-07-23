1e2db91bcb0e884c4280ca0f769391f0e6f87760
Joël Joly <joel.joly@gmail.com>
2017-07-23 21:53:33 +0200

    TEMP
    test, backup before upgrade

--- launchOpenVpn.sh
+++ launchOpenVpn.sh
@@ -4,7 +4,7 @@
 rm -R -f key_extract
 mkdir key_extract
 pushd key_extract
-python ../scripts/extract_ovpn_cert.py ../servers/vpngate_vpn536492275.opengw.net_udp_1394.ovpn
+python ../scripts/extract_ovpn_cert.py ../servers/vpngate_vpn741799636.opengw.net_udp_1472.ovpn
 #iptables -L -n -v –line-numbers
-sudo openvpn --config vpngate_nocert.ovpn
+sudo openvpn --config vpngate_nocert.ovpn --daemon VPN_instance
 popd
